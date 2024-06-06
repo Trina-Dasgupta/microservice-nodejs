6ca6b96a8d02fa0593313ae5da738ececf390cc3
Trina-Dasgupta-Official <trina.dasgupta@digitalaptech.co.in>
2024-06-06 19:50:13 +0530

    some changes

--- products/src/utils/index.js
+++ products/src/utils/index.js
@@ -82,7 +82,7 @@ module.exports.PublishShoppingEvent = async (payload) => {
 
 module.exports.CreateChannel = async () => {
   try {
-    // const connection = await amqplib.connect(MSG_QUEUE_URL);
+     const connection = await amqplib.connect(MSG_QUEUE_URL);
     const channel = await connection.createChannel();
     await channel.assertQueue(EXCHANGE_NAME, "direct", { durable: true });
     return channel;
